927957f2264f6246ddf4ab6ea9977a31c5d42d20
DaviMagalhaes <davimagal.es@gmail.com>
2020-01-25 01:52:09 -0300

    Evoluções sob sugestões do Pedro.

--- js/main-v1.2.js
+++ js/main-v1.2.js
@@ -44,29 +44,28 @@ $(function () {
     // Importar imagem.
     if(URL) {
         inputImage.change(function () {
-        var files = this.files
-        var file
+            let files = this.files
+            let file
 
-        if(!image.data('cropper')) {
-            return
-        }
-
-        if(files && files.length) {
-            file = files[0]
-
-            if(/^image\/\w+$/.test(file.type)) {
-
-                if(uploadedImageURL) {
-                    URL.revokeObjectURL(uploadedImageURL)
-                }
-
-                uploadedImageURL = URL.createObjectURL(file)
-                image.cropper('destroy').attr('src', uploadedImageURL).cropper(options)
-                inputImage.val('')
-            } else {
-                window.alert('Por favor, selecione uma imagem.')
+            if(!image.data('cropper')) {
+                return
             }
-        }
+
+            if(files && files.length) {
+                file = files[0]
+
+                if(/^image\/\w+$/.test(file.type)) {
+                    if(uploadedImageURL) {
+                        URL.revokeObjectURL(uploadedImageURL)
+                    }
+
+                    uploadedImageURL = URL.createObjectURL(file)
+                    image.cropper('destroy').attr('src', uploadedImageURL).cropper(options)
+                    inputImage.val('')
+                } else {
+                    window.alert('Por favor, selecione uma imagem.')
+                }
+            }
         })
     } else {
         inputImage.prop('disabled', true).parent().addClass('disabled')
@@ -149,55 +148,110 @@ $(function () {
             imageSmoothingQuality: 'high'
         })
 
-        $('#boxReferencia').hide()
+        $('#resultShow').hide() // Gerar referência e exibir resultado.
         if(canvas) {
-            $('#boxReferencia').show()
+            $('#resultShow').show() // davi
             gerarReferencia(canvas)
         }
     }
 
     // Gerar Referência: criar tabela com cada célula sendo um pixel e converte-a em imagem.
     function gerarReferencia(canvas) {
-        let context = canvas.getContext('2d')
+        let context = canvas.getContext('2d')   // Criar canvas da imagem.
         imgData = context.getImageData(0, 0, canvas.width, canvas.height)
         let d = imgData.data
 
-        let result = document.querySelector('#resultado')
-        let resultTable = result.querySelector('table')
-        if(resultTable) {
-            resultTable.remove()
+        let resultShow = document.querySelector('#resultShow')  // Checar referências anterior.
+        let tableRes = resultShow.querySelector('#ref')
+        if(tableRes) {
+            tableRes.remove()
         }
+        let tableLeg = resultShow.querySelector('#tabLeg')
+        tableLeg.querySelectorAll('tr.colorRef').forEach(function(elem) {
+            elem.remove()
+        })
 
-        let table = document.createElement('table')
-        table.cellPadding = 0
-        table.cellSpacing = 0
-        table.id = "ref"
+        tableRes = document.createElement('table') // Criar tabela para referências.
+        tableRes.cellPadding = 0
+        tableRes.cellSpacing = 0
+        tableRes.id = 'ref'
+        tableRes.classList.add('refQuad')
 
-        for(let y=0; y<imgData.height; y++) {
+        let colorList = []
+        let colorQuant = []
+        for(let y=0; y<imgData.height; y++) {   // Resultado: preencher tabela com as cores.
             let tr = document.createElement('tr')
             for(let x=0; x<imgData.width; x++) {
                 let td = document.createElement('td')
 
                 let i = (y * imgData.width + x) * 4
                 td.style.backgroundColor = 'rgba('+ [d[i], d[i+1], d[i+2], d[i+3]].join(',') +')'
+                let color = td.style.backgroundColor
 
+                let indexColor = colorList.indexOf(color)   // Listar cores para legenda.
+                if(indexColor == -1) {
+                    indexColor = 0
+                    colorList.push(color)
+                    colorQuant.push(1)
+                } else {
+                    colorQuant[indexColor]++
+                }
+
+                let text = document.createTextNode(indexColor + 1)
+                let span = document.createElement('span')
+                span.appendChild(text)
+                td.appendChild(span)
                 tr.appendChild(td)
             }
-            table.appendChild(tr)
+            tableRes.appendChild(tr)
         }
-        result.appendChild(table)
+        resultShow.querySelector('#result').appendChild(tableRes)
         redimensionarPixels()
 
-        let posScroll = $('#boxReferencia').offset().top    // Rolar para o resultado.
+        colorList.forEach(function(elem, index) {  // Resultado: preencher tabela da legenda.
+            let tr = document.createElement('tr')
+            tr.classList.add('colorRef')
+
+            let tdColor = document.createElement('td')
+            let div = document.createElement('div')
+            div.style.backgroundColor = elem
+            let b = document.createElement('b')
+            let text = document.createTextNode(index + 1)
+            b.appendChild(text)
+            div.appendChild(b)
+            tdColor.appendChild(div)
+            tr.appendChild(tdColor)
+
+            let tdPontos = document.createElement('td')
+            text = document.createTextNode(colorQuant[index])
+            tdPontos.appendChild(text)
+            tr.appendChild(tdPontos)
+
+            tableLeg.appendChild(tr)
+        })
+
+        let posScroll = $('#boxRef').offset().top    // Rolar para a referência.
         $('html, body').animate({scrollTop: posScroll})
     }
 
     // Redimensionar os pixels da Referência.
     $(window).resize(redimensionarPixels)
     function redimensionarPixels() {
-        let resultWidth = $('#resultado').width()
+        let resultWidth = $('#result').width()
         let pixelDim = resultWidth / imgData.width
 
         $('#ref td').width(pixelDim).height(pixelDim)
     }
+
+    // Alternar visão.
+    $('#btnVisao').click(function() {
+        let tableRef = $('#ref')
+        if(tableRef.hasClass('refQuad')) {
+            tableRef.removeClass('refQuad')
+            tableRef.addClass('refCruz')
+        } else {
+            tableRef.removeClass('refCruz')
+            tableRef.addClass('refQuad')
+        }
+    })
 })
